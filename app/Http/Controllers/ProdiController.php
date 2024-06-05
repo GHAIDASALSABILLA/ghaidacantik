16bcc63bedca614db082a0d970c931f7eae6a8e9
GHAIDA SALSABILLA <ghaida.gdsb@gmail.com>
2024-06-05 15:16:54 +0800

    CRUD PRODI

--- app/Http/Controllers/ProdiController.php
+++ app/Http/Controllers/ProdiController.php
@@ -30,9 +30,42 @@ class ProdiController extends Controller
             [
                 'nama_prodi' => 'required|unique:prodi|max:255'
             ],
+            [
+                'nama_prodi.required' => 'Nama Prodi nya di isi sayang',
+                'nama_prodi.unique' => 'Nama Prodi sudah ada cintaku',
+                'nama_prodi.max' => 'Nama Prodi maksimal 255 karakter ya cantikku',
+            ]
         );
             Prodi::create($validateData);
             return redirect ('/prodi');
+    }
+    public function edit(String $id)
+    {
+        $data = ['nama' => 'ghaida', 'foto' =>'girl.jpg'];
+        $prodi = Prodi::find($id);
+        return view('prodi.edit', compact(['data', 'prodi']));
+    }
+
+    public function update(Request $request, string $id)
+    {
+        $validateData = $request->validate(
+        [
+            'nama_prodi' => 'required|unique:prodi|max:255'
+        ],
+        [
+            'nama_prodi.required' => 'Nama Prodi nya di isi sayang',
+            'nama_prodi.unique' => 'Nama Prodi sudah ada cintaku',
+            'nama_prodi.max' => 'Nama Prodi maksimal 255 karakter ya cantikku',
+        ]
+        );
+    Prodi::where('id', $id)->update($validateData);
+    return redirect('/prodi');
+    }
+
+    public function destroy(string $id)
+    {
+        Prodi::destroy($id);
+        return redirect('/prodi');
     }
 }
 
